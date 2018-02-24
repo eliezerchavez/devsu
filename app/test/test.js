c5c3835babb1c66d18803ef46c5d4f28da6a1845
Eliezer E Chavez <eliezer.chavez@gmail.com>
2018-02-24 15:02:28 -0400

    Jenkins Tests

--- app/test/test.js
+++ app/test/test.js
@@ -1,3 +1,25 @@
+/*!
+ * Copyright (c) 2018 Eliezer Chavez <eliezer.chavez@gmail.com>
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
 process.env.NODE_ENV = "test";
 
 //Require the dev-dependencies
@@ -6,7 +28,8 @@ let chaiHttp = require("chai-http");
 let server = require("../");
 let should = chai.should();
 let os = require("os");
-let myReg = new RegExp("Hello Eliezer from " + os.hostname());
+let sucMsg = new RegExp("Hello Eliezer from " + os.hostname());
+let errMsg = /Only valid names are accepted!/;
 
 chai.use(chaiHttp);
 //Our parent block
@@ -22,7 +45,7 @@ describe("App", () => {
                 res.should.have.status(200);
 		        res.body.should.be.an.instanceOf(Object).and.have.property("message");
 	            res.body.message.should.be.a("string");
-                res.body.message.should.match(myReg);
+                res.body.message.should.match(sucMsg);
               done();
             });
       });
@@ -35,7 +58,7 @@ describe("App", () => {
               res.should.have.status(200);
               res.body.should.be.an.instanceOf(Object).and.have.property("message");
               res.body.message.should.be.a("string");
-              res.body.message.should.match(/Only valid names are accepted!/);
+              res.body.message.should.match(errMsg);
             done();
           });
     });
